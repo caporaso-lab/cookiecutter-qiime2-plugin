3fc80d6386dc48997a8a053496e1c999255cfbe2
Liz Gehret <elizabeth.gehret@gmail.com>
2024-08-27 11:50:07 -0700

    IMP: transition to pyproject.toml

--- {{ cookiecutter.package_name }}/setup.py
+++ {{ cookiecutter.package_name }}/setup.py
@@ -6,31 +6,10 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 import versioneer
 
-description = ("A template QIIME 2 plugin.")
-
 setup(
-    name="{{ cookiecutter.package_name }}",
     version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    license="BSD-3-Clause",
-    packages=find_packages(),
-    author="{{ cookiecutter.author_name }}",
-    author_email="{{ cookiecutter.author_email }}",
-    description=description,
-    url="{{ cookiecutter.project_url }}",
-    entry_points={
-        "qiime2.plugins": [
-            "{{ cookiecutter.module_name }}="
-            "{{ cookiecutter.module_name }}"
-            ".plugin_setup:plugin"]
-    },
-    package_data={
-        "{{ cookiecutter.module_name }}": ["citations.bib"],
-        "{{ cookiecutter.module_name }}.tests": ["data/*"],
-    },
-    zip_safe=False,
 )
