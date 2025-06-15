8d4b448ef6bde96a5aea51255ac2630e275c5636
Liz Gehret <elizabeth.gehret@gmail.com>
2025-06-14 21:01:15 -0600

    remove call to data.yaml

--- extensions/context.py
+++ extensions/context.py
@@ -1,16 +1,6 @@
-import urllib.request
 import subprocess
 
-import yaml
 from copier_templates_extensions import ContextHook
-
-# Download the data only once (because of sys.module cache),
-# instead of on each template event.
-DIST_DATA = \
-    'https://raw.githubusercontent.com/qiime2/distributions/dev/data.yaml'
-with urllib.request.urlopen(DIST_DATA) as response:
-    yaml_file = response.read()
-    distributions_data = yaml.safe_load(yaml_file)
 
 
 def _check_git_installed():
