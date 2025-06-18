d31ad00ac4b67473163958ea69ca55b9aaaac90c
Evan Bolyen <ebolyen@gmail.com>
2025-06-18 16:29:25 -0700

    cookiecutter to Copier transition (#32)
    * cookiecutter -> Copier transition
    * setup.py -> pyproject.toml
    * adds release env GHA, removes calls to target epoch
    * fully remove all references to target_epoch/dev_target
    * update ci to utilize dev env & use qiime2-cutlery GHA
    
    ---------
    
    Co-authored-by: Liz Gehret <elizabeth.gehret@gmail.com>
    Co-authored-by: Greg Caporaso <jgcap@fastmail.com>

--- template/{{ package_name }}/{{ module_name }}/__init__.py
+++ template/{{ package_name }}/{{ module_name }}/__init__.py
@@ -1,8 +1,13 @@
 # flake8: noqa
 # ----------------------------------------------------------------------------
-# Copyright (c) 2024, {{ cookiecutter.author_name }}.
+# Copyright (c) 2024, {{ author_name }}.
 #
 # Distributed under the terms of the Modified BSD License.
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
+
+try:
+    from ._version import __version__
+except ModuleNotFoundError:
+    __version__ = '0.0.0+notfound'
